7cae23dc70444c9de28766e454c124f53d8862d7
Gad NiYOGiSUBiZO <offgad200@gmail.com>
2026-08-11 19:47:13 +0200

    project setup arrange file in proper folder structure

---
+++ app/store/in_memory_store.py
@@ -0,0 +1,15 @@
+books = [
+    {
+        "id": "a1b2c3",
+        "title": "Clean Code",
+        "author": "Robert C. Martin",
+        "isbn": "9780132350884",
+        "genre": "Programming",
+        "totalCopies": 3,
+        "availableCopies": 2,
+    }
+]
+
+users = []
+
+loans = []
